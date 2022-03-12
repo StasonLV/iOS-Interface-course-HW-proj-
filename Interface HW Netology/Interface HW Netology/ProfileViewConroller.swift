4f9e2cae0fe2876512572b1180c3ac97be2de96d
StasonLV <talemane@gmail.com>
2022-03-12 20:37:10 +0300

    Added TabBar with controllers

---
+++ Interface HW Netology/Interface HW Netology/ProfileViewConroller.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewConroller.swift
+//  Interface HW Netology
+//
+//  Created by Stanislav Lezovsky on 12.03.2022.
+//
+
+import UIKit
+
+class ProfileViewConroller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray2
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
