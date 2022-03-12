15db0d07b53e859dd715205cb07a60d278253078
StasonLV <talemane@gmail.com>
2022-03-12 20:22:50 +0300

    Created clean project

---
+++ Interface HW Netology/Interface HW Netology/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Interface HW Netology
+//
+//  Created by Stanislav Lezovsky on 12.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
