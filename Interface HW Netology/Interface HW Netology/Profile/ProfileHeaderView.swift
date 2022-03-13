c8c4314917b7cb04b583910b0c7124e2c45e13a0
StasonLV <talemane@gmail.com>
2022-03-13 20:22:51 +0300

    Created button and constraints for UI elemnts

--- Interface HW Netology/Interface HW Netology/Profile/ProfileHeaderView.swift
+++ Interface HW Netology/Interface HW Netology/Profile/ProfileHeaderView.swift
@@ -8,70 +8,99 @@
 import UIKit
 
 class ProfileHeaderView: UIView {
-
-    var avatarImage: UIImageView?
+    var avatarImageView: UIImageView?
     var nameLabel: UILabel?
     var statusLabel: UILabel?
     var statusButton: UIButton?
     
+    func setupView() {
+            avatarImageView = UIImageView()
+            avatarImageView?.layer.masksToBounds = true
+            avatarImageView?.layer.cornerRadius = 80
+            avatarImageView?.layer.borderWidth = 3
+            avatarImageView?.layer.borderColor = UIColor.white.cgColor
+            addSubview(avatarImageView!)
+
+            nameLabel = UILabel()
+            nameLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+            nameLabel?.textColor = .black
+            addSubview(nameLabel!)
+
+            statusLabel = UILabel()
+            statusLabel?.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+            statusLabel?.textColor = .gray
+            addSubview(statusLabel!)
+        
+            statusButton = UIButton()
+            statusButton?.setTitle("Show status", for: .normal)
+            statusButton?.frame.size = CGSize(width: 50, height: 50)
+            statusButton?.layer.cornerRadius = 4
+            statusButton?.backgroundColor = .systemBlue
+            statusButton?.layer.cornerRadius = 4
+            statusButton?.layer.shadowRadius = 4
+            statusButton?.layer.shadowOpacity = 0.7
+            statusButton?.layer.shadowOffset = CGSize(width: 4,
+                                                  height: 4)
+            statusButton?.addTarget(self, action: #selector(self.printStatus), for: .touchUpInside)
+        statusButton?.addAction(
+            UIAction { _ in
+                print("Status")
+            }, for: .touchUpInside
+        )
+            addSubview(statusButton!)
+        
+        }
+    
+    @objc func printStatus(sender: UIButton!) {
+        print("Status")
+    }
+    
     
     override init(frame: CGRect) {
-        super .init(frame: frame)
+            super.init(frame: frame)
+
+            setupView()
+            setupConstraints()
         
-        viewCreation()
     }
+
+        required init?(coder: NSCoder) {
+            super.init(coder: coder)
+
+            setupView()
+        }
+      
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    
-    func viewCreation() {
-        avatarImage = UIImageView()
-        avatarImage?.layer.cornerRadius = 32
-        addSubview(avatarImage!)
-        
-        nameLabel = UILabel()
-        nameLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
-        addSubview(nameLabel!)
-        
-        statusLabel = UILabel()
-        statusLabel?.font = UIFont.systemFont(ofSize: 14, weight: .regular)
-        addSubview(statusLabel!)
-        
-        statusButton = UIButton()
-        statusButton?.backgroundColor = .systemBlue
-        statusButton?.layer.cornerRadius = 4
-        addSubview(statusButton!)
-    }
+    func setupConstraints() {
+        avatarImageView!.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel!.translatesAutoresizingMaskIntoConstraints = false
+        statusLabel!.translatesAutoresizingMaskIntoConstraints = false
+        statusButton!.translatesAutoresizingMaskIntoConstraints = false
     
     
-    func constraintsCreation() {
-        avatarImage?.translatesAutoresizingMaskIntoConstraints = false
-        nameLabel?.translatesAutoresizingMaskIntoConstraints = false
-        statusLabel?.translatesAutoresizingMaskIntoConstraints = false
-        statusButton?.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+        avatarImageView!.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 16),
+        avatarImageView!.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
+        avatarImageView!.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor),
+        avatarImageView!.widthAnchor.constraint(equalTo: avatarImageView!.heightAnchor, constant: 10),
+
+        nameLabel!.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 27),
+        nameLabel!.leadingAnchor.constraint(equalTo: avatarImageView!.trailingAnchor, constant: 34),
+        nameLabel!.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
+        nameLabel!.bottomAnchor.constraint(equalTo: avatarImageView!.centerYAnchor),
+
+        statusLabel!.leadingAnchor.constraint(equalTo: avatarImageView!.trailingAnchor, constant: 34),
+        statusLabel!.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
+        statusLabel!.topAnchor.constraint(equalTo: avatarImageView!.centerYAnchor),
+        statusLabel!.bottomAnchor.constraint(equalTo: statusButton!.topAnchor, constant: 34),
         
-        NSLayoutConstraint.activate([
-            avatarImage!.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor),
-            avatarImage!.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
-            avatarImage!.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor),
-            avatarImage!.widthAnchor.constraint(equalTo: avatarImage!.heightAnchor),
-        
-            nameLabel!.leadingAnchor.constraint(equalToSystemSpacingAfter: avatarImage!.trailingAnchor, multiplier: 1.0),
-            nameLabel!.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
-            nameLabel!.bottomAnchor.constraint(equalTo: avatarImage!.centerYAnchor),
-            
-            statusLabel!.leadingAnchor.constraint(equalToSystemSpacingAfter: avatarImage!.trailingAnchor, multiplier: 1.0),
-            statusLabel!.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
-            statusLabel!.topAnchor.constraint(equalTo: avatarImage!.centerYAnchor),
-            
-            statusButton!.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
-            statusButton!.topAnchor.constraint(equalToSystemSpacingBelow: avatarImage!.bottomAnchor, multiplier: 16.0),
-            statusButton!.widthAnchor.constraint(equalTo: statusButton!.heightAnchor),
-            statusButton!.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor)
-        
-        ])
+        statusButton!.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
+        statusButton!.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16),
+        statusButton!.topAnchor.constraint(equalTo: avatarImageView!.bottomAnchor, constant: 16),
+        statusButton!.heightAnchor.constraint(equalToConstant: 50)
+
+    ])
     }
 }
+
 
--- Interface HW Netology/Interface HW Netology/Profile/ProfileViewConroller.swift
+++ Interface HW Netology/Interface HW Netology/Profile/ProfileViewConroller.swift
@@ -9,34 +9,21 @@ import UIKit
 
 class ProfileViewConroller: UIViewController {
     
-    let profileHeader: ProfileHeaderView = {
-        let view = ProfileHeaderView()
-        view.frame = CGRect(
-            x: 50,
-            y: 60,
-            width: 300,
-            height: 300
-        )
-        view.avatarImage?.image = UIImage(named: "avatar")
-        view.nameLabel?.text = "Stas Lezovsky"
-        view.statusLabel?.text = "Listening to..."
-        
-        return view
-    }()
-
+    let profileHeader = ProfileHeaderView(frame: .zero)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .lightGray
-        
-        
-        let profileHeader = ProfileHeaderView(frame: .zero)
-        profileHeader.avatarImage?.image = UIImage(named: "avatar")
-        profileHeader.nameLabel?.text = "Stas Lezovsky"
-        profileHeader.statusLabel?.text = "Listening to..."
-
+        profileHeader.avatarImageView?.image = UIImage(named: "avatar.jpg")
+        profileHeader.nameLabel?.text = "Stanislav Lezovsky"
+        profileHeader.statusLabel?.text = "Waiting for something..."
         view.addSubview(profileHeader)
-        
-    }
 
+    }
     
+    override func viewWillLayoutSubviews() {
+        profileHeader.frame = CGRect(x: 0, y: 20, width: view.frame.size.width, height: 250)
+    }
 }
+    
