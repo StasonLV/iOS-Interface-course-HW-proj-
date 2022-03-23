ea0ddd32b0265f01ba82f1a91f716c67bd0e3333
StasonLV <talemane@gmail.com>
2022-03-23 19:09:10 +0300

    comitting line change

--- Interface HW Netology/Interface HW Netology/Profile/ProfileViewConroller.swift
+++ Interface HW Netology/Interface HW Netology/Profile/ProfileViewConroller.swift
@@ -14,6 +14,7 @@ class ProfileViewConroller: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .lightGray
         profileHeader.avatarImageView.image = UIImage(named: "avatar.jpg")
         profileHeader.nameLabel.text = "Stanislav Lezovsky"
